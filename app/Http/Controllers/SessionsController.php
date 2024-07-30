1cdd33f0740ee49f10ecaa9d92420b4eb86b5244
krlos <pckrlos@gmail.com>
2024-07-30 19:04:02 -0400

    added method to loggin and get menu info

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
 
 use Illuminate\Http\Request;
 
 class SessionsController extends Controller
 {
     public function login(Request $request){
-        dd('asdfasdfasdf');
         return view('session.login');
     }
 
@@ -22,9 +22,30 @@ class SessionsController extends Controller
         ]);
         //dd(Hash::make('test'));
         $credentials = $request->only('username', 'password');
-        //dd($credentials);
+        
         if(Auth::attempt($credentials)){
             session()->regenerate();
+            // $users = DB::table('users')->get();
+            $query = "
+                select r.id as rolId, r.rol, p.id as permissionId, p.permission, m.id as menuId, m.menu
+                    from users u 
+                        inner join users_roles ur on (u.id =  ur.user_id )
+                        inner join roles r on (ur.rol_id = r.id)
+                            inner join roles_permissions rp on (r.id = rp.rol_id)
+                              inner join permissions p on (rp.permission_id = p.id)
+                                inner join menus m on (p.menu_id = m.id)                              
+                                where u.username = '".$request->username."';
+
+            ";
+            $objMenu = DB::select(DB::raw($query)->getValue(DB::getQueryGrammar()));
+            $arrMenu = array();
+
+            foreach ($objMenu as $key => $value) {
+                $arrMenu[$value->rol][] = $value->permission;
+            }
+            
+            \Session::put('arrMenu', $arrMenu);
+            
             return redirect('dashboard')->with(['success'=>'You are logged in.']);
         }else{
             // return redirect()->intended('/');
--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -9,180 +9,37 @@
                     <ul id="sidebarnav">
 
 
-                        <li class="sidebar-item">
-                            <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
-                              <span class="d-flex">
-                                <i class="ti ti-layout-grid"></i>
-                              </span>
-                              <span class="hide-menu">UI Elements</span>
-                            </a>
-                            <ul aria-expanded="false" class="collapse first-level">
-                              <li class="sidebar-item">
-                                <a href="../main/ui-accordian.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Accordian</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-badge.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Badge</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-buttons.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Buttons</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-dropdowns.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Dropdowns</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-modals.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Modals</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-tab.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Tab</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-tooltip-popover.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Tooltip &amp; Popover</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-notification.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Alerts</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-progressbar.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Progressbar</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-pagination.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Pagination</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-typography.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Typography</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-bootstrap-ui.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Bootstrap UI</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-breadcrumb.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Breadcrumb</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-offcanvas.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Offcanvas</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-lists.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Lists</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-grid.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Grid</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-carousel.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Carousel</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-scrollspy.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Scrollspy</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item">
-                                <a href="../main/ui-spinner.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Spinner</span>
-                                </a>
-                              </li>
-                              <li class="sidebar-item mb-3">
-                                <a href="../main/ui-link.html" class="sidebar-link">
-                                  <div class="round-16 d-flex align-items-center justify-content-center">
-                                    <i class="ti ti-circle"></i>
-                                  </div>
-                                  <span class="hide-menu">Link</span>
-                                </a>
-                              </li>
-                            </ul>
-                          </li>
+
+
               
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="index.html" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span
                                     class="hide-menu">Dashboard</span></a></li>
+
+                        
+                    @foreach (@session('arrMenu') as $mainItem => $menu)                        
+                        <li class="sidebar-item">
+                            <a class="sidebar-link has-arrow" href="javascript:void(0)" aria-expanded="false">
+                              <span class="d-flex">
+                                <i class="mdi mdi-view-dashboard"></i>
+                              </span>
+                              <span class="hide-menu">{{ $mainItem }}</span>
+                            </a>
+                            <ul aria-expanded="false" class="collapse first-level">
+                            @foreach ($menu as $column => $value)
+                              <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">
+                                  <div class="round-16 d-flex align-items-center justify-content-center">
+                                    <i class="ti ti-circle"></i>
+                                  </div>
+                                  <span class="hide-menu">{{ $value }}</span>
+                                </a>
+                              </li>
+                              @endforeach
+                            </ul>
+                        </li>          
+                        @endforeach  
+                        {{--
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="pages-profile.html" aria-expanded="false"><i
                                     class="mdi mdi-account-network"></i><span class="hide-menu">Profile</span></a></li>
@@ -198,6 +55,7 @@
                         <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link"
                                 href="error-404.html" aria-expanded="false"><i class="mdi mdi-alert-outline"></i><span
                                     class="hide-menu">404</span></a></li>
+                         --}}  
                         
                     </ul>
 
